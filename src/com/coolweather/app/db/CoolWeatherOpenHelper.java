cda75f684bd4c5fbdf51a08e6be7933e33bba964
ZZ <746717241@qq.com>
2016-03-29 14:50:56 +0800

    完成遍历省市县三级列表的功能

--- src/com/coolweather/app/db/CoolWeatherOpenHelper.java
+++ src/com/coolweather/app/db/CoolWeatherOpenHelper.java
@@ -11,44 +11,40 @@ public class CoolWeatherOpenHelper extends SQLiteOpenHelper {
 	 *  Province表建表语句
 	 */
 	public static final String CREATE_PROVINCE = "create table Province ("
-				+ "id integer primary key autoincrement, " //自增长
-				+ "province_name text, "                   //省名字
-				+ "province_code text)";                   //省级代号
+				+ "id integer primary key autoincrement, " 
+				+ "province_name text, "
+				+ "province_code text)";
 	/**
 	 *  City表建表语句
 	 */
 	public static final String CREATE_CITY = "create table City ("
-				+ "id integer primary key autoincrement, "    //自增长
-				+ "city_name text, "                          //市名
-				+ "city_code text, "                          //市级代号
-				+ "province_id integer)";                     //City表 关联 Province表 的外键
+				+ "id integer primary key autoincrement, " 
+				+ "city_name text, " 
+				+ "city_code text, " 
+				+ "province_id integer)";
 	/**
 	 *  County表建表语句
 	 */
 	public static final String CREATE_COUNTY = "create table County ("
-				+ "id integer primary key autoincrement, "    //自增长
-				+ "county_name text, "                        //县名
-				+ "county_code text, "                        //县级代号
-				+ "city_id integer)";                         //County表 关联 City表 的外键
+				+ "id integer primary key autoincrement, " 
+				+ "county_name text, " 
+				+ "county_code text, " 
+				+ "city_id integer)";
 
-	//构造函数
 	public CoolWeatherOpenHelper(Context context, String name, CursorFactory factory,
 			int version) {
 		super(context, name, factory, version);
 	}
 
-	
-	//创建数据库db，其中包含3张表
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		db.execSQL(CREATE_PROVINCE);  // 创建Province表
-		db.execSQL(CREATE_CITY);      // 创建City表
-		db.execSQL(CREATE_COUNTY);    // 创建County表
+		db.execSQL(CREATE_CITY);  // 创建City表
+		db.execSQL(CREATE_COUNTY);  // 创建County表
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		
 	}
 
 }
